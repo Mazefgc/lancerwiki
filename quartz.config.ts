2320d4bda1e3823d5450663c32e534f7b2467770
Mazefgc <lucasrocha.xlucas04@gmail.com>
2025-07-07 16:28:49 -0300

    Quartz sync: Jul 7, 2025, 4:28 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -46,7 +46,7 @@ const config: QuartzConfig = {
           darkgray: "#d4d4d4",
           dark: "#ebebec",
           secondary: "#ba4040",
-          tertiary: "#84a59d",
+          tertiary: "#ba4040",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
         },

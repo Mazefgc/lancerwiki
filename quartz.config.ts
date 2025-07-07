a83a5fe5e10d02744ff0b3a353493f70e1fe19bd
Mazefgc <lucasrocha.xlucas04@gmail.com>
2025-07-07 15:49:57 -0300

    Quartz sync: Jul 7, 2025, 3:49 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "mazefgc.github.io/lancerwiki/",
     ignorePatterns: ["private", ".obsidian"],
     defaultDateType: "modified",
     theme: {
